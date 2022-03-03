9e057ac42aa5cd8b0e3af8df704100279b561ea6
dkimd5 <dkimd5@mail.ru>
2022-03-03 12:11:57 +0300

    Add import for Chats

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import { ChatEngine } from "react-chat-engine";
+import { auth } from "../firebase";
 
 function Chats() {
   return <div>Chats</div>;
