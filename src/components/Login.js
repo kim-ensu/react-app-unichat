031ceee03c1651b511d829c54d8d23ec3ee479af
dkimd5 <dkimd5@mail.ru>
2022-03-03 11:02:25 +0300

    Add sign in buttons

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,21 @@ import React from "react";
 import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <div id="login-page">
+      <div id="login-card">
+        <h2>Wellcome to Unichat!</h2>
+        <div className="login-button google">
+          <GoogleOutlined /> Sign In with Google
+        </div>
+        <br />
+        <br />
+        <div className="login-button google">
+          <FacebookOutlined /> Sign In with Facebook
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
