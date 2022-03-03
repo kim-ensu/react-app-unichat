fea8c68b81104668d435d64f041477d0c3f2ddce
dkimd5 <dkimd5@mail.ru>
2022-03-03 12:17:56 +0300

    Add jsx to Chats

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -4,7 +4,21 @@ import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
 
 function Chats() {
-  return <div>Chats</div>;
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">Uhichat</div>
+        <div className="logout-tab">Logout</div>
+      </div>
+
+      <ChatEngine
+        height="calc(100vh - 66px)"
+        projectId="e92b4cad-c679-4ccd-8b3e-14d249e3296f"
+        userName="."
+        userSecret="."
+      />
+    </div>
+  );
 }
 
 export default Chats;
