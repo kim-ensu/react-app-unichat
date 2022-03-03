6c43a7dbc9cba1eb4fc95bc2b3e92fa0bf92a96d
dkimd5 <dkimd5@mail.ru>
2022-03-03 11:50:30 +0300

    Add auth redirec onclick

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,13 +3,16 @@ import { GoogleOutlined } from "@ant-design/icons";
 import "firebase/app";
 
 import { auth } from "../firebase";
+import firebase from "firebase/app";
 
 function Login() {
   return (
     <div id="login-page">
       <div id="login-card">
         <h2>Wellcome to Unichat!</h2>
-        <div className="login-button google">
+        <div
+          className="login-button google"
+          onCLick={() => auth.signInWithRedirect(new firebase.auth.GoogleAuthProvider())}>
           <GoogleOutlined /> Sign In with Google
         </div>
       </div>
