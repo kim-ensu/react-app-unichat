93872503f26b8ba8b482a1151e41f0719c1cf4ad
dkimd5 <dkimd5@mail.ru>
2022-03-04 09:56:13 +0300

    Add request of user profile

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { ChatEngine } from "react-chat-engine";
 import { auth } from "../firebase";
 
 import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
 
 function Chats() {
   const history = useHistory();
   const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
 
   const handleLogout = async () => {
     await auth.signOut();
-
     history.push("/");
   };
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+      return;
+    }
+    axios
+      .get("httpls://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": "e92b4cad-c679-4ccd-8b3e-14d249e3296f",
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      });
+  }, [user, history]);
 
   return (
     <div className="chats-page">
